ea7c5bd63fb4fb12b742e89463516c1ef93cf848
Sefat Siddiquea Sifa <63462931+sifa123@users.noreply.github.com>
2021-06-27 14:28:41 +0600

    Update register.php

--- citrix-employee-project/register.php
+++ citrix-employee-project/register.php
@@ -51,8 +51,6 @@ if (isset($_POST['submit'])) {
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-	<link rel="stylesheet" type="text/css" href="style.css">
-
 	<title>Register Form</title>
 	<style>
 		@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
